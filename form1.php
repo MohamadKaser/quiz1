b7c406e469e24c5df5c8065dc53de02fed3fd295
Mohamad kaser <Mohamadkaser@outlook.com>
2021-03-20 11:04:00 +0300

    This is my First form which i used the GET method!

---
+++ form1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CSE0409 First Quiz</title>
+</head>
+<body>
+    
+    <h1>HTML Forms</h1>
+
+    <form method="get">
+        <p>
+            <label for="form1">first input</label>
+            <input type="text" name="form1" id="forms">
+        </p>
+        <input type="button" value="send">
+    </form>
+
+<pre>
+<?php GET:
+    print_r($_GET);
+?>
+</pre>
+
+</body>
+</html>
