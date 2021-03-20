1b032776231d32283e2bd248f0b0e3618ae8136b
Mohamad kaser <Mohamadkaser@outlook.com>
2021-03-20 11:11:38 +0300

    This is my Second form which i used the POST method!

---
+++ form2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CSE0409 First Quiz</title>
+</head>
+<body>
+    
+    <h1>HTML Forms</h1>
+
+    <form method="post">
+        <p>
+            <label for="form1">first input</label>
+            <input type="text" name="form1" id="forms">
+        </p>
+        <input type="button" value="send">
+    </form>
+
+<pre>
+<?php POST:
+    print_r($_POST);
+?>
+</pre>
+
+</body>
+</html>
